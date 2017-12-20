Fixed image scaling

Camera/network/h5importer.py
@@ -14,9 +14,8 @@ class HDF5Importer():
         self.data, self.labels = self.convert(raw_data, raw_labels)
         self.length = len(self.data)
         print("shape: {}".format(self.data.shape))
-
         if image_name:
-            self.plot_samples(np.random.randint(0, self.length, 9), image_name)
+        	self.plot_samples(np.random.randint(0, self.length, 9), image_name)
 
 
     def convert(self, data, labels):
@@ -27,9 +26,9 @@ class HDF5Importer():
         arrImgs = np.zeros([self.dataSet_size, 28, 28], dtype='float32')
 
         for index in range(self.dataSet_size):
-            currentImg = data[index]
+            currentImg = np.true_divide(data[index], 255.0)
             currentLabel = labels[index]
-            arrImgs[index] = np.true_divide(currentImg.reshape([28,28]), 255.0)
+            arrImgs[index] = currentImg.reshape([28,28])
             arrLabels[index][currentLabel] = 1.0
 
         return arrImgs, arrLabels
@@ -42,7 +41,7 @@ class HDF5Importer():
         for row in ax:
             for a in row:
                 a.imshow(self.data[numbers[counter]])
-                a.set_title('Label: {1} (sample {1})'.format(self.labels[numbers[counter]].argmax(), numbers[counter]))
+                a.set_title('Label: {0} (sample {1})'.format(self.labels[numbers[counter]].argmax(), numbers[counter]))
                 counter += 1
         plt.show(block=False)
         plt.savefig(image_name, bbox_inches='tight')

Camera/network/network.py
@@ -70,7 +70,7 @@ class Network():
         self.y = tf.nn.softmax(tf.nn.relu(tf.matmul(self.h_drop2, self.weights['fc2']) + self.biases['fc2']))
 
 
-        self.mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+        self.mnist = input_data.read_data_sets('/tmp/MNIST_data', one_hot=True)
 
     def conv2d(self, x, W):
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -82,7 +82,7 @@ class Network():
         '''
         Trains the model, and saves the parameters into a checkpoint
         '''
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y))
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y_, logits=self.y))
         train_step = tf.train.AdamOptimizer(parameters['learning_rate']).minimize(cross_entropy)
         accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_,1)), tf.float32))
 