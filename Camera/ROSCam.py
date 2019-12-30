22fdc42ee5ce91614f3c55d5a5ddefd653e67cf3
Nacho Condés <nachocmstrato@gmail.com>
2019-12-30 16:16:33 +0100

    Now the camera serves rosbag files as well.

--- Camera/ROSCam.py
+++ Camera/ROSCam.py
@@ -1,68 +1,87 @@
 #
-# Created on Jan, 2019
+# Created on Dec, 2019
 #
 # @author: naxvm
 #
-# Class which abstracts a RGBD Camera from ROS messages,
-# and provides the methods to keep it constantly refreshed.
+# Class to abstract a RGBD Camera into OpenCV images,
+# Provides the methods to keep it constantly refreshed.
 
-import traceback
 import threading
 import numpy as np
 import rospy
 from sensor_msgs.msg import Image
 import cv_bridge
 import cv2
+import time
+import rosbag
 
 IMAGE_HEIGHT = 480
 IMAGE_WIDTH = 640
 
-class NotAdvertisedException(Exception):
-    ''' Raised when the RGB/depth topics have not been already advertised. '''
-    pass
-
 class ROSCam:
 
-    def __init__ (self, topics):
-        ''' Camera class gets images from live video and transform them
-        in order to predict the digit in the image.
+    def __init__ (self, topics, rosbag_path=None):
+        ''' Camera class gets new images from the ROS topics or a recorded rosbag
+        and convert them into OpenCV format, offering the latest one to the caller.
 
         A control thread is not necessary (the subscribers are controlled
         by rospy threads).
         '''
+        
+        self.use_bag = rosbag_path is not None
 
-        # rospy.init_node('ROSCam', anonymous=True)
-        # Check the existance of the topics
+        if self.use_bag is not None:
+            # Create iterators for the rosbag
+            self.bag = rosbag.Bag(rosbag_path)
+            self.rgb_iter = self.bag.read_messages(topics['RGB'])
+            self.depth_iter = self.bag.read_messages(topics['Depth'])
+        else:
+            # Wait for the topics to be advertised
+            topic_names, _ = map(list, zip(*rospy.get_published_topics()))
+            while topics['RGB'] not in topic_names or topics['Depth'] not in topic_names:
+                print('Waiting for the topics to be advertised...')
+                time.sleep(1)
+                topic_names, _ = map(list, zip(*rospy.get_published_topics()))
 
-        topic_names, _ = map(list, zip(*rospy.get_published_topics()))
-        if topics['rgb'] not in topic_names or topics['depth'] not in topic_names:
-            raise NotAdvertisedException
+            # Subscribers
+            self.rgb_lst = rospy.Subscriber(topics['RGB'], Image, self.__rgbCallback, queue_size=1)
+            self.d_lst = rospy.Subscriber(topics['Depth'], Image, self.__depthCallback, queue_size=1)
 
         # Two bridges for concurrency issues
         self.rgb_bridge = cv_bridge.CvBridge()
         self.depth_bridge = cv_bridge.CvBridge()
 
         # Placeholders
-        self.rgb_img = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH))
-        self.depth_img = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH))
-
-        # Subscribers
-        self.rgb_lst = rospy.Subscriber(topics['rgb'], Image, self.__rgbCallback, queue_size=1)
-        self.d_lst = rospy.Subscriber(topics['depth'], Image, self.__depthCallback, queue_size=1)
+        self.__rgb_data = None
+        self.__depth_data = None
 
         self.lock = threading.Lock()
-        # rospy.spin()
 
 
-    def __rgbCallback(self, rgb_img):
+    def __rgbCallback(self, rgb_data):
         self.lock.acquire()
-        self.rgb_img = self.rgb_bridge.imgmsg_to_cv2(rgb_img, rgb_img.encoding)
+        self.__rgb_data = rgb_data
         rospy.logdebug("RGB updated")
         self.lock.release()
 
-    def __depthCallback(self, depth_img):
+    def __depthCallback(self, depth_data):
         self.lock.acquire()
-        self.depth_img = self.depth_bridge.imgmsg_to_cv2(depth_img, depth_img.encoding)
+        self.__depth_data = depth_data
         rospy.logdebug("Depth updated")
         self.lock.release()
         
+    def getImages(self):
+        ''' Return the latest images from a rosbag or from the topic. '''
+        if self.use_bag:
+            kk = next(self.rgb_iter)
+            # print(type(kk), len(kk))
+            _, rgb_data, _ = next(self.rgb_iter)
+            _, depth_data, _ = next(self.depth_iter)
+        else:
+            rgb_data = self.__rgb_data
+            depth_data = self.__depth_data
+
+        rgb_image = self.rgb_bridge.imgmsg_to_cv2(rgb_data, rgb_data.encoding)
+        depth_image = self.depth_bridge.imgmsg_to_cv2(depth_data, depth_data.encoding)
+
+        return rgb_image, depth_image
