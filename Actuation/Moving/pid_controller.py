Fixed error computation.

Actuation/Moving/pid_controller.py
@@ -5,7 +5,7 @@ from cprint import cprint
 class PIDController:
     '''Given the desired gains for each component, create a PID controller to
     compute an appropriate response for the reference person.'''
-    def __init__(self, Kp, Ki, Kd, K_loss, limit, stop_range):
+    def __init__(self, Kp, Ki, Kd, K_loss, limit, stop_range, verbose):
         # Specific parameters
         self.Kp = Kp
         self.Ki = Ki
@@ -14,6 +14,7 @@ class PIDController:
         self.K_loss = K_loss
         self.limit = limit
         self.stop_range = stop_range
+        self.center = np.mean(stop_range)
 
         self.lost_limiter = limit / 3.0
 
@@ -22,6 +23,7 @@ class PIDController:
         self.cumulative_error = 0
         self.prev_response = 0
 
+        self.verbose = verbose
 
     def lostResponse(self):
         '''Stop with a controlled inertia.'''
@@ -33,8 +35,12 @@ class PIDController:
         return error >= self.stop_range[0] and error <= self.stop_range[1]
 
 
-    def computeResponse(self, error, verbose=False):
+    def computeResponse(self, error):
         '''Compute the appropriate response.'''
+
+        if np.isnan(error):
+            return self.last_response
+
         if self.isInRange(error):
             # Reset the memory and stop moving.
             self.prev_error = 0
@@ -42,17 +48,20 @@ class PIDController:
             self.last_response = 0
             cprint.ok('\tNull response (target in range)')
             return 0
-        # Compute the response otherwise
-        P = self.Kp * error
-        I = self.Ki * (error + self.cumulative_error)
+
+        # Compute the response from
+        # the relative error otherwise
+        rel_error = error - self.center
+        P = self.Kp * rel_error
+        I = self.Ki * (rel_error + self.cumulative_error)
         if abs(self.prev_error) > 0: # To avoid jumps because of the huge derivative
-            D = self.Kd * (error - self.prev_error)
+            D = self.Kd * (rel_error - self.prev_error)
         else:
             D = 0
 
         response = P + I + D
 
-        if verbose: # Print outputs
+        if self.verbose: # Print outputs
             cprint.info(f'\tP >> {P:.3f}')
             cprint.info(f'\tI >> {I:.3f}')
             cprint.info(f'\tD >> {D:.3f}')
@@ -64,8 +73,8 @@ class PIDController:
             response = np.sign(response) * self.limit
 
         # Update parameters for the next iteration
-        self.prev_error = error
-        self.cumulative_error += error
+        self.prev_error = rel_error
+        self.cumulative_error += rel_error
         self.last_response = response
         # Returning the response value
         return response

utils.py
@@ -55,7 +55,7 @@ def computeWError(coords, im_width):
     of the tracked person.'''
     person_center = coords[0] + coords[2]/2
 
-    w_error = person_center - im_width/2
+    w_error = im_width/2 - person_center
     return w_error
 
 def computeXError(coords, depth):
@@ -75,5 +75,9 @@ def computeXError(coords, depth):
     # Sample the values and compute the median depth
     sampled_depths = cropped_depth[grid].ravel()
     median = np.nanmedian(sampled_depths)
-
+    if np.isnan(median):
+        # The person is too close to estimate the distance
+        # We report 0 distance
+        return 0.0
     return median
+